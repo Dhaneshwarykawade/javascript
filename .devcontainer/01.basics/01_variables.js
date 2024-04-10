1584f2c49d9e673322be032f26d261cb4b3f19b7
Reina09 <144926397+Dhaneshwarykawade@users.noreply.github.com>
2024-04-10 16:08:57 +0000

    container config done

---
+++ .devcontainer/01.basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId =12233445
+let accountEmail = "Dhaneshwary@google.com"
+var accountPassword = "45678"
+accountCity = "Jaipur"
+
+// accountId = 2 not allowed
+
+console.log(accountId);
